d61d6886be322e4eba2ab668e841ba64a3213de2
anchit1501 <1501.anchit@gmail.com>
2019-05-30 20:08:45 +0530

    BoilerPlate Ready

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import HomePage from './home/HomePage';
+import AboutPage from './about/AboutPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-fluid">
+    <Route exact path="/" component={HomePage}/>
+    <Route exact path="/about" component={AboutPage}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,8 @@
+import React from'react';
+const AboutPage=()=>
+    <div>
+    <h1>About</h1>
+    <p>This app uses React, Redux aand React Router, and many other helpful libraries.</p>
+    </div>
+
+export default AboutPage;
